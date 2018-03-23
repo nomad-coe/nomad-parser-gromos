323f4bd9d6dcc7c4a2f8666c7a55d058fc80522b
Berk Onat <b.onat@warwick.ac.uk>
2018-03-23 13:33:12 +0000

    Typo in test definitions

--- src/test/scala/eu/nomad_lab/parsers/GromosParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/GromosParserSpec.scala
@@ -5,10 +5,10 @@ import org.specs2.mutable.Specification
 object GromosParserTests extends Specification {
   "GromosParserTest" >> {
     "[Peptide in Water with 2792 atoms] test with json-events" >> {
-      ParserRun.parse(NAMDParser, "parsers/gromos/test/examples/eq/eq_peptide_1.omd", "json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(GromosParser, "parsers/gromos/test/examples/eq/eq_peptide_1.omd", "json-events") must_== ParseResult.ParseSuccess
     }
     "[Peptide in Water with 2792 atoms] test with json" >> {
-      ParserRun.parse(NAMDParser, "parsers/gromos/test/examples/eq/eq_peptide_1.omd", "json") must_== ParseResult.ParseSuccess
+      ParserRun.parse(GromosParser, "parsers/gromos/test/examples/eq/eq_peptide_1.omd", "json") must_== ParseResult.ParseSuccess
     }
   }
 }
