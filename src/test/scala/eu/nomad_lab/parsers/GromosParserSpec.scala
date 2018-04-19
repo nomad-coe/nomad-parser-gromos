f4d872f163fd52ad89fe644c01a0006004f3cd62
Berk Onat <b.onat@warwick.ac.uk>
2018-04-19 17:39:36 +0100

    Changing scala test setting from concurrent threads to serial threads

--- src/test/scala/eu/nomad_lab/parsers/GromosParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/GromosParserSpec.scala
@@ -3,10 +3,12 @@ package eu.nomad_lab.parsers
 import org.specs2.mutable.Specification
 
 object GromosParserTests extends Specification {
-  "GromosParserTest" >> {
+  "GromosParserTest_1" >> {
     "[Peptide in Water with 2792 atoms] test with json-events" >> {
       ParserRun.parse(GromosParser, "parsers/gromos/test/examples/eq/eq_peptide_1.omd", "json-events") must_== ParseResult.ParseSuccess
     }
+  }
+  "GromosParserTest_2" >> {
     "[Peptide in Water with 2792 atoms] test with json" >> {
       ParserRun.parse(GromosParser, "parsers/gromos/test/examples/eq/eq_peptide_1.omd", "json") must_== ParseResult.ParseSuccess
     }
