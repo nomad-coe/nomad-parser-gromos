75cd80dd27664d275e567bce223314891706fb03
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-08-27 01:14:34 +0200

    Adapted further metainfo changes

--- gromosparser/metainfo/__init__.py
+++ gromosparser/metainfo/__init__.py
@@ -16,4 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import gromosparser.metainfo.gromos
+from nomad.metainfo import Environment
+
+from . import gromos
+
+m_env = Environment()
+m_env.m_add_sub_section(Environment.packages, gromos.m_package)
